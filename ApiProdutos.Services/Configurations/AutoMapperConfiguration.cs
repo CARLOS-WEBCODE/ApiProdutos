b7bfb2ca6b45f333cc31283efd0f5969aeeba427
Antonio Barbosa <acrb1108@gmail.com>
2024-10-25 18:32:42 -0300

    Incluindo AutoMapper

---
+++ ApiProdutos.Services/Configurations/AutoMapperConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProdutos.Services.Configurations;
+
+public static class AutoMapperConfiguration
+{
+    public static void AddAutoMapper(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    }
+}
