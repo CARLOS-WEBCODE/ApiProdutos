a396ef716a481ac456bfcdf883c0c1743f68de7f
Antonio Barbosa <acrb1108@gmail.com>
2024-10-26 00:07:11 -0300

    Adicionando HttpPost Controller ProdutoPostRequest

---
+++ ApiProdutos.Services/Controllers/ProdutosController.cs
@@ -0,0 +1,40 @@
+﻿using ApiProdutos.Infra.Data.Entities;
+using ApiProdutos.Infra.Data.Interfaces;
+using ApiProdutos.Services.Requests;
+using ApiProdutos.Services.Responses;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProdutos.Services.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly IProdutoRepository _produtoRepository;
+    private readonly IMapper _mapper;
+
+    public ProdutosController(IProdutoRepository produtoRepository, IMapper mapper)
+    {
+        _produtoRepository = produtoRepository;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult Post(ProdutoPostRequest request)
+    {
+        try
+        {
+            var produto = _mapper.Map<Produto>(request);
+
+            _produtoRepository.Add(produto);
+
+            var response = _mapper.Map<ProdutoGetResponse>(produto);
+            return StatusCode(201, response);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = ex.Message });
+        }
+    }
+}
