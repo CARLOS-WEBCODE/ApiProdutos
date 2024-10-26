c142b08b3a2b2df86cdd5d99fbb8f695510e049f
Antonio Barbosa <acrb1108@gmail.com>
2024-10-26 02:35:45 -0300

    Incluindo a classe ProdutosTest

---
+++ ApiProdutos.Tests/TestCases/ProdutosTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiProdutos.Tests.TestCases;
+public class ProdutosTest
+{
+    [Fact(Skip = "Não implementado.")]
+    public void Post_Produtos_Return_Ok()
+    {
+
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Put_Produtos_Return_Ok()
+    {
+
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Put_Produtos_Return_UnprocessableEntity()
+    {
+
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Delete_Produtos_Return_Ok()
+    {
+
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Delete_Produtos_Return_UnprocessableEntity()
+    {
+
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void GetAll_Produtos_Return_Ok()
+    {
+
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void GetById_Produtos_Return_Ok()
+    {
+
+    }
+}
