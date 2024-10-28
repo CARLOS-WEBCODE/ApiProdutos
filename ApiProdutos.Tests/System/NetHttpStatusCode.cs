ad49b25c2a12a994ecc484a7eef720ffc6c45ee8
Antonio Barbosa <acrb1108@gmail.com>
2024-10-28 01:22:20 -0300

    Teste

---
+++ ApiProdutos.Tests/System/NetHttpStatusCode.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace System;
+
+internal class NetHttpStatusCode
+{
+    public static HttpStatusCode Created { get; internal set; }
+}
--- ApiProdutos.Tests/TestCases/ProdutosTest.cs
+++ ApiProdutos.Tests/TestCases/ProdutosTest.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using ApiProdutos.Services.Requests;
+using ApiProdutos.Tests.Helpers;
+using Bogus;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -8,10 +15,41 @@ using Xunit;
 namespace ApiProdutos.Tests.TestCases;
 public class ProdutosTest
 {
-    [Fact(Skip = "Não implementado.")]
-    public void Post_Produtos_Return_Ok()
+    [Fact]
+    public async Task Post_Produtos_Return_Ok()
     {
+        var faker = new Faker("pt_BR");
 
+        #region Criando os dados da requisição
+
+        var request = new ProdutoPostRequest
+        {
+            Nome = faker.Commerce.ProductName(),
+            Preco = decimal.Parse(faker.Commerce.Price()),
+            Quantidade = 10
+        };
+
+        var content = new StringContent(JsonConvert.SerializeObject(request),Encoding.UTF8, "application/json");
+
+        #endregion
+
+        #region Executando o serviço de API
+
+        var client = new HttpClient();
+
+        var response = await client.PostAsync
+            ($"{ApiHelper.Endpoint}/Produtos", content);
+
+        #endregion
+
+        #region Validar o resultado do teste
+
+        response
+            .StatusCode
+            .Should()
+            .Be(HttpStatusCode.Created);
+
+        #endregion
     }
 
     [Fact(Skip = "Não implementado.")]
